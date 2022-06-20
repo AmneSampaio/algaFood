139a21c2a7f4afa5999bccb22d1ac75f809084f2
AmneSampaio <39910460+AmneSampaio@users.noreply.github.com>
2022-06-20 09:36:58 -0300

    Adding sql file to input default data on DB.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+insert into kitchen (id, name) values (1, 'Brazilian');
+insert into kitchen (id, name) values (2, 'Portuguese');
+insert into restaurant (name, delivery_fee, kitchen_id) values ('Bucho cheio','10.0', 1);
+insert into restaurant (name, delivery_fee, kitchen_id) values ('By Manuel', '5.0', 2);
+insert into state (name) values ('Amazonas');
+insert into state (name) values ('Rio de Janeiro');
+insert into city (name, state_id) values ('Manaus',1);
+insert into city (name, state_id) values ('São Gonçalo',2);
