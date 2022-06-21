11642f3ae2e1801a21f29a0a3c8feb1fb29ed214
AmneSampaio <39910460+AmneSampaio@users.noreply.github.com>
2022-06-21 07:20:20 -0300

    Removing unused imports

--- src/main/java/com/algaworks/ecommerce/api/controller/StateController.java
+++ src/main/java/com/algaworks/ecommerce/api/controller/StateController.java
@@ -1,10 +1,8 @@
 package com.algaworks.ecommerce.api.controller;
 
-import com.algaworks.ecommerce.domain.model.Kitchen;
 import com.algaworks.ecommerce.domain.model.State;
 import com.algaworks.ecommerce.domain.repository.StateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
