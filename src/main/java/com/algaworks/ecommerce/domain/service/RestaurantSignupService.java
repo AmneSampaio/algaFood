4fc6e2b75a2787ccabc383d97460842ae7e19769
AmneSampaio <39910460+AmneSampaio@users.noreply.github.com>
2022-06-21 06:24:05 -0300

    Adding a restaurant service

---
+++ src/main/java/com/algaworks/ecommerce/domain/service/RestaurantSignupService.java
@@ -0,0 +1,31 @@
+package com.algaworks.ecommerce.domain.service;
+
+import com.algaworks.ecommerce.domain.model.Restaurant;
+import com.algaworks.ecommerce.domain.repository.RestaurantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RestaurantSignupService {
+
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+
+    public List<Restaurant> all() {
+        return restaurantRepository.all();
+    }
+
+    public Restaurant byId(Long id) {
+        try {
+            return restaurantRepository.byId(id);
+        } catch (NullPointerException e) {
+            throw new NullPointerException("This id do not correspond to any restaurant in DB");
+        }
+    }
+
+    public Restaurant toSave(Restaurant restaurant) {
+        return restaurantRepository.toAdd(restaurant);
+    }
+}
