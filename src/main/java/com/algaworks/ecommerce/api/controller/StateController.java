7b7980425970c45893fdccf3a7b8eb3fb2be9445
AmneSampaio <39910460+AmneSampaio@users.noreply.github.com>
2022-06-25 11:28:50 -0300

    Adding endpoints GET, POST and PUT to state controller.

--- src/main/java/com/algaworks/ecommerce/api/controller/StateController.java
+++ src/main/java/com/algaworks/ecommerce/api/controller/StateController.java
@@ -1,23 +1,57 @@
 package com.algaworks.ecommerce.api.controller;
 
 import com.algaworks.ecommerce.domain.model.State;
-import com.algaworks.ecommerce.domain.repository.StateRepository;
+import com.algaworks.ecommerce.domain.service.StateService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/state")
+@RequestMapping("/states")
 public class StateController {
 
     @Autowired
-    private StateRepository stateRepository;
+    private StateService stateService;
 
     @GetMapping
     public List<State> toList() {
-        return stateRepository.all() ;
+        return stateService.all() ;
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<State> toSearch(@PathVariable Long id) {
+        State state = stateService.byId(id);
+
+        if (state != null) {
+            return ResponseEntity.ok(state);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<State> toAdd(@RequestBody State state) {
+        if (state != null) {
+            state = stateService.toSave(state);
+            return ResponseEntity.ok(state);
+        }
+
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> toChange(@PathVariable Long id, @RequestBody State state) {
+        State stateDB = stateService.byId(id);
+
+        if (stateDB != null ) {
+            BeanUtils.copyProperties(state,stateDB,"id");
+            state = stateService.toChange(stateDB);
+            return ResponseEntity.ok(state);
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
