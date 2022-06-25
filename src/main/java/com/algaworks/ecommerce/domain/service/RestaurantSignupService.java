0bdb800b1ee5d071e3a6054bf906ae679f3fd88a
AmneSampaio <39910460+AmneSampaio@users.noreply.github.com>
2022-06-25 12:28:03 -0300

    Adding method that deletes

--- src/main/java/com/algaworks/ecommerce/domain/service/RestaurantSignupService.java
+++ src/main/java/com/algaworks/ecommerce/domain/service/RestaurantSignupService.java
@@ -1,10 +1,14 @@
 package com.algaworks.ecommerce.domain.service;
 
+import com.algaworks.ecommerce.domain.exception.EntityInUseException;
 import com.algaworks.ecommerce.domain.model.Restaurant;
 import com.algaworks.ecommerce.domain.repository.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @Service
@@ -31,5 +35,19 @@ public class RestaurantSignupService {
 
     public Restaurant toChange(Restaurant restaurant) {
         return restaurantRepository.toAdd(restaurant);
+    }
+
+    public void toDelete(Long id) {
+
+        try {
+            restaurantRepository.toDelete(id);
+        } catch (EntityNotFoundException | EmptyResultDataAccessException e) {
+            throw new EntityNotFoundException(String.format("Not found restaurant related to the id %d.", id));
+        } catch (DataIntegrityViolationException e) {
+            throw new EntityInUseException(
+                    String.format("Restaurant of id %d can't be removed, " +
+                            "it has children or dependency with another " +
+                            "entity.", id));
+        }
     }
 }
--- src/main/java/com/algaworks/ecommerce/domain/service/StateService.java
+++ src/main/java/com/algaworks/ecommerce/domain/service/StateService.java
@@ -1,10 +1,14 @@
 package com.algaworks.ecommerce.domain.service;
 
+import com.algaworks.ecommerce.domain.exception.EntityInUseException;
 import com.algaworks.ecommerce.domain.model.State;
 import com.algaworks.ecommerce.domain.repository.StateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @Service
@@ -26,6 +30,21 @@ public class StateService {
         return stateRepository.toAdd(state);
     }
 
-    public State toChange(State state) { return stateRepository.toAdd(state);
+    public State toChange(State state) {
+        return stateRepository.toAdd(state);
+    }
+
+    public void toDelete(Long id) {
+
+        try {
+            stateRepository.toDelete(id);
+        } catch (EntityNotFoundException | EmptyResultDataAccessException e) {
+            throw new EntityNotFoundException(String.format("Not found state related to the id %d.", id));
+        } catch (DataIntegrityViolationException e) {
+            throw new EntityInUseException(
+                    String.format("State of id %d can't be removed, " +
+                            "it has children or dependency with another " +
+                            "entity.", id));
+        }
     }
 }
